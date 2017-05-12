a9315f9a96e0ce4c15a72d9e562875721d94aaae
Nicolas Galler <ngaller@gmail.com>
2017-05-12 10:07:03 -0400

    remove react stuff

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 module.exports = {
   "ecmaFeatures": {
-    "jsx": true,
     "modules": true
   },
   "plugins": [
-    "react"
   ],
   "rules": {
-    "react/jsx-uses-react": 2,
-    "react/jsx-uses-vars": 2,
-    "react/react-in-jsx-scope": 2,
     "no-labels": 0,
     "arrow-parens": 0,
     "comma-dangle": 0,
@@ -22,9 +17,6 @@ module.exports = {
     "keyword-spacing": 0,
     "no-unused-expressions": 0,
     "one-var": 0
-  },
-  "env": {
-    "browser": true
   },
   "extends": "standard",
   "parser": "babel-eslint"
